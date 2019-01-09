aac7b092b2148a051bc8adebe1fe9f40031ed55e
hellfirewar <hellfire_war@nate.com>
2019-01-09 19:14:19 +0900

    패스워드 암호화 Filter and Wrapper , IdSearchEND.jsp 추가

---
+++ src/com/brw/command/filter/EncrypFilter.java
@@ -0,0 +1,56 @@
+package com.brw.command.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import com.brw.command.filter.EncryptWrapper;
+
+/**
+ * Servlet Filter implementation class EncrypFilter
+ */
+@WebFilter({"/createUser.do", "/login.do"})
+public class EncrypFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public EncrypFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		//암호화래퍼객체 생성
+		EncryptWrapper encRequest = new EncryptWrapper((HttpServletRequest)request);
+		System.out.println("EncryptFilter처리 완료!");
+
+		//ServletRequest타입의 파라미터에  작성한 EncrytWrapper객체 전달
+		//ServletRequest - HttpServletRequest - HttpServletRequestWrapper - EncryptWrapper
+		chain.doFilter(encRequest, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
